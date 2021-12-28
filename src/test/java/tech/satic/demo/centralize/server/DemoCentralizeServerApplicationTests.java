554280f54dd747b4b61de4a50668d8b136bf83ea
lorincet <lorincet@gmail.com>
2021-12-28 10:07:43 +0100

    Update file

---
+++ src/test/java/tech/satic/demo/centralize/server/DemoCentralizeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.satic.demo.centralize.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCentralizeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
